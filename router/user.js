f7b8acd1b6635830c126d378be70d69e07f2ae33
junhee916 <junhee916@naver.com>
2021-08-05 18:09:54 +0900

    mysql_update&delete_clear

--- router/user.js
+++ router/user.js
@@ -9,7 +9,10 @@ router.get('/', async (req, res) => {
 
         res.status(200).json({
             msg : "get users",
-            userInfo : result[0]
+            userInfo : {
+                id : result.id,
+                result : result[0]
+            }
         })
     }
     catch(err){
@@ -25,7 +28,7 @@ router.post('/', (req, res) => {
     
     if(nickname && password){
         try{
-            db.promise().query(`INSERT INTO USERS VALUES ('${nickname}', '${password}');`)
+            db.promise().query(`INSERT INTO USERS (nickname, password) VALUES ('${nickname}', '${password}');`)
 
             res.status(200).json({
                 msg : "success signup"
@@ -39,6 +42,45 @@ router.post('/', (req, res) => {
     }
     
 
+})
+
+router.put('/:userId', async (req, res) => {
+
+    const id = req.params.userId
+
+    const nickname = req.body.nickname;
+
+    const sql  = "UPDATE users SET nickname = ? WHERE id = ?"
+
+    try{
+        const result = await db.promise().query(sql, [nickname, id])
+
+        res.status(200).json({
+            msg  : "update userInfo",
+            userInfo : result
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            msg : err.message
+        })
+    }
+})
+
+router.delete('/', async (req, res) => {
+
+    try{
+        await db.promise().query("delete from users")
+
+        res.status(200).json({
+            msg : "delete users"
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            msg : err.message
+        })
+    }
 })
 
 module.exports = router
